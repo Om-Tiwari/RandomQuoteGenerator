14960fb25120bb2a4985a7d3fd254c172dac6d77
Om Tiwari <omthegreat085@gmail.com>
2022-08-06 12:05:44 +0530

    Added Copy To Clipboard feature

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const btn = document.querySelector("#btn");
+const copyBtn = document.querySelector("#btn-copy");
 const quote = document.querySelector(".quote");
 const author = document.querySelector("#author");
+const toolTip = document.querySelector(".tooltiptext");
 
 btn.addEventListener("click", function () {
 	fetch("https://api.quotable.io/random")
 		.then((res) => res.json())
 		.then((result) => {
-			quote.innerText = result.content;
+			quote.textContent = result.content;
 			let nameUrl = result.author.replace(" ", "_");
 			author.innerHTML =
 				"<a id='author' href='https://en.wikipedia.org/wiki/" +
@@ -15,4 +17,17 @@ btn.addEventListener("click", function () {
 				result.author +
 				"</a>";
 		});
+});
+
+copyBtn.addEventListener("click", function () {
+	let copyText = '"' + quote.textContent + '"' + "~" + author.textContent;
+	if (copyText === '""~') {
+		return;
+	}
+	navigator.clipboard.writeText(copyText);
+	toolTip.innerHTML = "Copied";
+});
+
+copyBtn.addEventListener("mouseout", function () {
+	toolTip.innerHTML = "Copy to clipboard";
 });
